a156b4108574f15110b5ae1507f45d171b75e1c9
rashid mp <rashid.parambil@outlook.com>
2025-02-16 12:44:37 +0000

    added code for cloud run service public

--- infra-cloud-run/main.tf
+++ infra-cloud-run/main.tf
@@ -8,7 +8,26 @@ resource "google_cloud_run_v2_service" "default" {
   template {
     service_account = "cloud-run-deployment-dev@rp-dev-450309.iam.gserviceaccount.com"
     containers {
-      image = "us-central1-docker.pkg.dev/rp-dev-450309/rp-dev-net-repo/dotnet8-api:04ce075"
+      image = "us-central1-docker.pkg.dev/rp-dev-450309/rp-dev-net-repo/dotnet8-api:d4d1956"
+      startup_probe {
+        http_get {
+          path = "/health"
+          port = 8080
+        }
+        initial_delay_seconds = 10
+        period_seconds        = 5
+        failure_threshold     = 30
+      }
     }
   }
 }
+
+resource "google_cloud_run_service_iam_member" "noauth" {
+  project        = google_cloud_run_v2_service.default.project
+  location       = google_cloud_run_v2_service.default.location
+  service        = google_cloud_run_v2_service.default.name
+  role           = "roles/run.invoker"
+  member         = "allUsers"
+}
+
+
